cd29cfa45dc5d3a8873460ee5990de51a09057c1
Alex Trzeciak <trebek08@gmail.com>
2018-04-17 12:34:11 -0700

    added hard coded data that isnt present over API endpoint

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Card = props => {
 	const { product, ratingsWithFilters, sentimentWithFilters } = props.data;
@@ -10,9 +11,9 @@ const Card = props => {
 	    	<div className="topImage"> <img className="imageStyle" src={product.images[0].desktop} /> </div>
 	    	<div className="topData">
 	    	 <div>{product.name}</div>
-	    	 <div>
-	    	 	<span> {product.price} </span> 
-	    	 	<span> {product.colors} </span> 
+	    	 <div className="bubbleContainer">
+	    	 	<span className="bubble"> {product.price} </span> 
+	    	 	<span className="bubble"> {product.colors} </span> 
 	    	 </div>
 	    	 </div>
 	    	<div className="topPercentile"> 
@@ -22,13 +23,35 @@ const Card = props => {
 	    	</div>
 	    </div>
 	    <div className="section middleSection">
+	    <div className="detractors"> 
+	    	<div> Detractors</div>
+	    	<div> 2% </div>
+	    </div>
+	    <div className="graph">
+	    	<div> graph</div>
+	    </div>
+	    <div className="advocates">
+	    	<div>Advocates</div>
+	    	<div> 47% </div>
+	    </div>
 
 	    </div>
 	    <div className="section thirdSection">
-
+	    	<div className="score rightBorder">
+	    		<div className="sphere">96</div>
+	    		<div className="smallWords"> Millenials </div>
+	    	</div>
+	    	<div className="score rightBorder">
+	    		<div className="sphere">86</div>
+	    		<div className="smallWords"> Fashion Forward </div>
+	    	</div>
+	    	<div className="score">
+	    		<div className="sphere">68</div>
+	    		<div className="smallWords"> Repeat Customers </div>
+	    	</div>
 	    </div>
 	    <div className=" section bottomSection">
-
+	    	<div className="linkContainer"> <Link to="/"> View More </Link></div>
 	    </div>
 	  </div>
 	)};
